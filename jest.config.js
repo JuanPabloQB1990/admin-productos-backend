c4697c3b3392137601f7d7ba37ef129fa63e39a2
Juan Pablo <juanpabloqb1990@gmail.com>
2026-02-03 17:50:22 -0500

    test fixed 1.4 bd test and dev created

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["D:/Proyectos/admin-productos/administrador-productos-backend/src/data/index.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/data/index.ts"],
 };
