60dd6ed3c4fe1912b8beaa8e989920f0e22c59e9
Juan Pablo <juanpabloqb1990@gmail.com>
2026-03-02 20:43:32 -0500

    deploy railway 1.0

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import server, { connectDatabase } from "./server";
 import colors from "colors"
 
-const PORT = process.env.PORT || 3000
+const PORT = Number(process.env.PORT) || 3000
 
 async function startServer() {
-  await connectDatabase()
-  server.listen(PORT, () => {
-    console.log(colors.cyan.bold(`http://localhost:${PORT}`));
-  })
+  try {
+    await connectDatabase()
+    server.listen(PORT, "0.0.0.0", () => {
+      console.log(colors.cyan.bold(`http://localhost:${PORT}`));
+    })
+    
+  } catch (error) {
+    
+  }
 }
 
 startServer()
